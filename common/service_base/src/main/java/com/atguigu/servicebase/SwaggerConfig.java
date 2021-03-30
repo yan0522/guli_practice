4d54b3e065c37b638f76358743c2c4c1541e5afe
yp <yp_jobs@163.com>
2021-03-30 23:01:51 +0800

    feat(pom):新增依赖 feat(EduApplication):新增包扫描 feat(common):新增swagger配置

---
+++ common/service_base/src/main/java/com/atguigu/servicebase/SwaggerConfig.java
@@ -0,0 +1,66 @@
+package com.atguigu.servicebase;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * TODO
+ *
+ * @ClassName: SwaggerConfig
+ * @author: yan
+ * @since: 2021/3/30 22:37
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                //.securityContexts(Arrays.asList(securityContexts()))
+                //.securitySchemes(Arrays.asList(securitySchemes()))
+                .select()
+                //.paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("网站-课程中心API文档")
+                .description("本文档描述了课程中心微服务接口定义")
+                .version("1.0")
+                .contact(new Contact("java", "http://atguigu.com", "112233@163.com"))
+                .build();
+    }
+
+    /*private SecurityScheme securitySchemes() {
+        return new ApiKey("token", "token", "header");
+    }
+
+    private SecurityContext securityContexts() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("token", authorizationScopes));
+    }*/
+}
--- service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * TODO
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.atguigu"})
 public class EduApplication {
 
     public static void main(String[] args) {

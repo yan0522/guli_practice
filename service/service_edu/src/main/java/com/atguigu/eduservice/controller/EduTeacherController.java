d64d8e4a6d55f3b7e34a6fe4a93b06128a55d243
yp <yp_jobs@163.com>
2021-03-30 13:01:53 +0800

    Initial commit

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author yp
+ * @since 2021-03-29
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+

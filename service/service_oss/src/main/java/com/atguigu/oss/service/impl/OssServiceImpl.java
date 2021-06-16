59fb6e9c7b97d89ae1ad4973afe0beab5cad4a03
yp <yp_jobs@163.com>
2021-06-16 22:33:39 +0800

    fix(OssServiceImpl.java):修复上传文件会覆盖的bug

--- service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
@@ -4,10 +4,12 @@ import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.atguigu.oss.service.OssService;
 import com.atguigu.oss.utils.ConstantPropertiesUtils;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 /**
  * TODO
@@ -29,6 +31,13 @@ public class OssServiceImpl implements OssService {
         try {
             //获取文件名称
             String fileName = file.getOriginalFilename();
+            //文件名拼接随机数防同名覆盖
+            String uuid = UUID.randomUUID().toString().replaceAll("-","");
+            fileName = uuid + fileName;
+            //按日期建立文件夹存储
+            String dataPath = new DateTime().toString("yyyy/MM/dd");
+
+            fileName = dataPath + "/" + fileName;
             //创建oss对象
             OSS ossClient = new OSSClientBuilder().build(endpoint,accessKeyId,accessKeySecret);
             //获取上传文件的输入流

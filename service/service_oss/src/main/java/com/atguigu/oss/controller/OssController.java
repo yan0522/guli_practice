fa25c934602aa91e83b6270db56550223ad5665f
yp <yp_jobs@163.com>
2021-06-16 20:16:44 +0800

    feat(service_oss):新增oss模块，添加文件上传功能

---
+++ service/service_oss/src/main/java/com/atguigu/oss/controller/OssController.java
@@ -0,0 +1,33 @@
+package com.atguigu.oss.controller;
+
+import com.atguigu.commonutils.R;
+import com.atguigu.oss.service.OssService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * TODO
+ *
+ * @ClassName: OssController
+ * @author: yan
+ * @since: 2021/6/16 16:56
+ */
+@RestController
+@RequestMapping("/eduoss/fileoss")
+public class OssController {
+
+    @Autowired
+    private OssService ossService;
+
+    //上传头像的方法
+    @PostMapping
+    public R uploadOssFile(MultipartFile file) {
+
+        //获取上传文件,返回文件路径
+       String url = ossService.uploadFileAvatar(file);
+        return R.ok().data("url",url);
+    }
+}
---
+++ service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
@@ -0,0 +1,52 @@
+package com.atguigu.oss.service.impl;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.atguigu.oss.service.OssService;
+import com.atguigu.oss.utils.ConstantPropertiesUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+/**
+ * TODO
+ *
+ * @ClassName: OssServiceImpl
+ * @author: yan
+ * @since: 2021/6/16 16:56
+ */
+@Service
+public class OssServiceImpl implements OssService {
+    @Override
+    public String uploadFileAvatar(MultipartFile file) {
+
+        String endpoint = ConstantPropertiesUtils.END_POINT;
+        String accessKeyId = ConstantPropertiesUtils.KEY_ID;
+        String accessKeySecret = ConstantPropertiesUtils.KEY_SECRET;
+        String bucketName = ConstantPropertiesUtils.BUCKETNAME;
+
+        try {
+            //获取文件名称
+            String fileName = file.getOriginalFilename();
+            //创建oss对象
+            OSS ossClient = new OSSClientBuilder().build(endpoint,accessKeyId,accessKeySecret);
+            //获取上传文件的输入流
+            InputStream inputStream = file.getInputStream();
+
+            //调用oss方法实现上传
+            ossClient.putObject(bucketName,fileName,inputStream);
+            //关闭ossClient
+            ossClient.shutdown();
+            //把上传之后文件的路径返回
+            //需要手动拼接
+            String url = "http://"+bucketName+"."+endpoint+"/"+fileName;
+            return url;
+
+        }catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
